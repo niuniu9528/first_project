8ba4f426198ee19b787ed435fc9d9914d58068ef
niuniu9528 <99157479+niuniu9528@users.noreply.github.com>
2022-02-07 12:03:59 +0800

    Add a new priject
    This is a good test

---
+++ ChairMan.java
@@ -0,0 +1,10 @@
+class  ChairMan{
+	
+	public static void main(String[] args){
+	
+	System.out.println("姓名：小明");
+	System.out.println();
+	System.out.println("性别：男");
+	System.out.println("家庭住址：翻斗花园58号");
+		}
+}
